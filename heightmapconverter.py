f53c66380bb5373384f87e7bc77002bb25948521
Rakete175 <64165408+Rakete175@users.noreply.github.com>
2020-11-03 02:40:29 +0100

    adding colourmap support

--- heightmapconverter.py
+++ heightmapconverter.py
@@ -7,8 +7,9 @@ It is meant for mapmakers, because many tools for this stuff are either broken o
 
 
 How it works:
-* replace path with the one of your heightmap, including the image itself
-* include this into your config file
+* replace the first path with the one of your heightmap, including the image itself
+* replace second path with the complete path of your colourmap.
+* mention this script in your config.toml file (e.g. with piqueserver.scripts.heightmapconverter)
 * open terminal
 * type piqueserver --copy-config to refresh it to the new config file
 * type piqueserver to start the server and wait a moment
@@ -18,8 +19,8 @@ How it works:
 * now you have the .vxl somewhere on your pc. If you are using Aos it is C:/Ace of Spades/vxl/lastsav.vxl, on OpenSpades it is called shot0001.vxl for example (OpenSpades tells you, just search it).
 
 If your map looks strange it can have two reasons:
-* colourmap not yet implemented, but is planned
 * your heightmap needs to be inverted first
+* this code may have bugs
 
 Warning: NEVER implement this in a public server. If you use the /draw command, everyone disconnects
 '''
@@ -30,7 +31,8 @@ Warning: NEVER implement this in a public server. If you use the /draw command, 
 from piqueserver.commands import command
 import PIL
 from PIL import Image
-img = Image.open('paste-your-path-with-picturefile-here').convert('RGB')
+img = Image.open('paste-your-path-with-heightmap-file-here').convert('RGB')
+img2 = Image.open('paste-your-path-with-colourmap-file-here').convert('RGB')
 xwidth, yheight = img.size
 
 @command('draw')
@@ -48,10 +50,11 @@ def apply_script(protocol, connection, config):
                 print('set 512 x 512 pixels pls before doing this')
             else:
                 pixels=img.load()
+                pixels2=img2.load()
                 for x in range(xwidth):
                     for y in range(yheight):
-                        (r,g,b) = (pixels[x, y])
-                        h=(r+b+g)/12
+                        (r,g,b) = (pixels[x, y])        
+                        h=(r+b+g)/12-1
                         for z in range(64):
                             if z>=h:
                                 solid=True
@@ -59,7 +62,8 @@ def apply_script(protocol, connection, config):
                                 solid=False
                             colour=(r,g,b)
                             if solid:
-                                map.set_point(x,y,z,(r,g,b))
+                                (rcolour, gcolour, bcolour) = (pixels2[x, y])
+                                map.set_point(x,y,z,(rcolour, gcolour, bcolour))
                             else:
                                 map.remove_point(x,y,z)
                 print('ok done')
